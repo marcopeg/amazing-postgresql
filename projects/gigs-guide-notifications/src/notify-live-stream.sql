6f42924e4fe78a61c42df4597dff2d263842ae04
marcopeg <marco.pegoraro@gmail.com>
2021-08-16 17:01:39 +0200

    Refactor function

--- projects/gigs-guide-notifications/src/notify-live-stream.sql
+++ projects/gigs-guide-notifications/src/notify-live-stream.sql
@@ -7,39 +7,18 @@ RETURNS TABLE (
 AS $$
 BEGIN
   RETURN QUERY
-  WITH
-    "events_followed_by_users" as (
-        SELECT
-        "t1"."user_id",
-        "t1"."artist_id",
-        "t2"."event_id"
-        FROM "users_performers" as "t1"
-        JOIN "events_performers_temp" as "t2" on "t1"."artist_id" = "t2"."artist_id"
-    )
-  , "events_by_country_code" as (
+  SELECT
+    "t1"."user_id"::text as "_user_id", 
+    array_agg("t3"."event_id") as "_event_ids"
+  FROM "users_performers" as "t1"
+  JOIN "events_performers_temp" as "t2" on "t1"."artist_id" = "t2"."artist_id"
+  JOIN "all_music_events_temp" as "t3" on "t2"."event_id" = "t3"."event_id"
+  WHERE "t3"."country_code" = 'XX'
+    AND NOT EXISTS (
       SELECT 
-      "t1".*, 
-      "t2"."country_code" 
-      FROM "events_followed_by_users" as "t1"
-      JOIN "all_music_events_temp" as "t2" on "t1"."event_id" = "t2"."event_id"
-      WHERE "t2".country_code = 'XX'
-  )
-  , "users_events_notifications" as (
-      SELECT
-      "t1"."user_id",
-      "t1"."event_id",
-      "t1"."country_code"
-      FROM "events_by_country_code" as "t1"
-      WHERE NOT EXISTS (
-        SELECT 
-        FROM "users_sent_events" as "t2"
-        WHERE "t2"."event_id" = "t1"."event_id" and "t2"."user_id" = "t1"."user_id"
+      FROM "users_sent_events" as "t4"
+      WHERE "t4"."event_id" = "t2"."event_id" and "t4"."user_id" = "t1"."user_id"
     )
-  )
-  SELECT 
-  "t1"."user_id"::text as "_user_id", 
-  array_agg("t1"."event_id") as "_event_ids"
-  FROM "users_events_notifications" as "t1"
   GROUP BY "t1"."user_id";
 
 END; $$
