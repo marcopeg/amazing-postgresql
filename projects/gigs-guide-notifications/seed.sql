11c13053f632f78b5a55caa42b1e71acd06b2a7a
marcopeg <marco.pegoraro@gmail.com>
2021-08-21 10:01:38 +0200

    Improve seeding

--- projects/gigs-guide-notifications/seed.sql
+++ projects/gigs-guide-notifications/seed.sql
@@ -4,16 +4,24 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- Generate seeding settings into a temporary table
 CREATE TEMPORARY TABLE "seed_settings" ("doc" JSONB);
 INSERT INTO "seed_settings" VALUES ('{
-  "users_count": 10000,
-  "artists_count": 10000,
-  "events_count": 10000,
+  "users_count": 100,
+  "artists_count": 1000,
+  "events_count": 50000,
 
   "artists_id_max": 99999999,
   "events_id_max": 99999999,
 
   "users_performers_min": 0,
-  "users_performers_max": 25,
-  "users_gaps_perc": 0.3
+  "users_performers_max": 50,
+
+  "events_performers_min": 0,
+  "events_performers_max": 8,
+  
+  "sent_events_min": 0,
+  "sent_events_max": 25,
+
+  "users_gaps_perc": 0.3,
+  "events_gaps_perc": 0.3
 }'::jsonb);
 
 -- Generate random users (temporary table)
@@ -60,7 +68,6 @@ ON CONFLICT ON CONSTRAINT "all_music_events_temp_pkey" DO NOTHING;
 -- INSERT RELATIONS
 -- USERS > ARTIST
 --
-
 WITH
   "seed_info" AS (
   SELECT
@@ -133,6 +140,74 @@ FROM "insert_values";
 
 
 
+--
+-- INSERT RELATIONS
+-- ARTIST > EVENT
+--
+WITH
+  "seed_info" AS (
+  SELECT
+    (
+      SELECT ("doc"->'events_performers_min')::INT FROM "seed_settings"
+    ) AS "following_min"
+  , (
+      SELECT ("doc"->'events_performers_max')::INT FROM "seed_settings"
+    ) AS "following_max"
+  , (
+      (SELECT ("doc"->'events_performers_max')::INT FROM "seed_settings")
+      +
+      (SELECT ("doc"->'events_performers_max')::INT FROM "seed_settings")
+      *
+      (SELECT ("doc"->'events_gaps_perc')::REAL FROM "seed_settings")
+    )::INT AS "following_random_pool"
+  , (
+      SELECT ("doc"->'artists_count')::int FROM "seed_settings"
+    ) AS "prog_rand_max"
+)
+, "aggregated_values" AS (
+  SELECT
+    "event_id" AS "target",
+    (
+      SELECT array_agg("artist_id")
+      FROM (
+        SELECT "artist_id"
+        FROM (
+          SELECT (
+            0 + trunc(random() * (SELECT "prog_rand_max" FROM "seed_info"))::int
+          ) AS "prog_id"
+          FROM generate_series(1, (SELECT "following_random_pool" FROM "seed_info"))
+        ) AS "gs1"
+
+        JOIN "artists_tmp" USING ("prog_id")
+        WHERE "t1"."event_id" = "t1"."event_id"
+
+        LIMIT (
+          floor(random() * ((
+            SELECT "following_max" FROM "seed_info"
+          ) - (
+            SELECT "following_min" FROM "seed_info"
+          ) + 1) + (
+            SELECT "following_min" FROM "seed_info"
+          ))
+        )
+      ) AS "sq1"
+    ) AS "values"
+  FROM "all_music_events_temp" AS "t1"
+)
+, "insert_values" AS (
+  INSERT INTO "public"."events_performers_temp"
+  SELECT
+    "target" AS "event_id"
+  , "value" AS "artist_id"
+  FROM "aggregated_values", unnest("values") AS "value"
+  ON CONFLICT ON CONSTRAINT "events_performers_temp_event_id_artist_id"
+  DO NOTHING
+  RETURNING *
+)
+-->> Output >>
+SELECT COUNT(*) AS "inserted_events_artists"
+FROM "insert_values"
+;
 
 
 
@@ -150,71 +225,88 @@ FROM "insert_values";
 
 
 
--- Populate relation "UsersPerformers"
--- TRUNCATE "users_performers";
--- INSERT INTO "users_performers"
--- SELECT
---   (
---     SELECT "user_id" 
---     FROM "users_tmp" 
---     WHERE "gs1" = "gs1" 
---     ORDER BY random()
---     LIMIT 1 
---   ) AS "user_id",
---   (
---     SELECT "artist_id" 
---     FROM "artists_tmp" 
---     WHERE "gs1" = "gs1" 
---     ORDER BY random()
---     LIMIT 1 
---   ) AS "artist_id"
--- FROM generate_series(1, 1000) "gs1"
--- ON CONFLICT ON CONSTRAINT "users_performers_pkey"
--- DO NOTHING;
 
--- Populate relation "EventsPerformers"
--- TRUNCATE "events_performers_temp";
--- INSERT INTO "events_performers_temp"
--- SELECT
---   (
---     SELECT "event_id" 
---     FROM "all_music_events_temp" 
---     WHERE "gs1" = "gs1" 
---     ORDER BY random()
---     LIMIT 1 
---   ) AS "event_id",
---   (
---     SELECT "artist_id" 
---     FROM "artists_tmp" 
---     WHERE "gs1" = "gs1" 
---     ORDER BY random()
---     LIMIT 1 
---   ) AS "artist_id"
--- FROM generate_series(1, 1000) "gs1"
--- ON CONFLICT ON CONSTRAINT "events_performers_temp_event_id_artist_id"
--- DO NOTHING;
+--
+-- INSERT RELATIONS SENT EVENTS
+-- USER > EVENT
+--
+WITH
+  "seed_info" AS (
+  SELECT
+    (
+      SELECT ("doc"->'sent_events_min')::INT FROM "seed_settings"
+    ) AS "following_min"
+  , (
+      SELECT ("doc"->'sent_events_max')::INT FROM "seed_settings"
+    ) AS "following_max"
+  , (
+      (SELECT ("doc"->'sent_events_max')::INT FROM "seed_settings")
+      +
+      (SELECT ("doc"->'sent_events_max')::INT FROM "seed_settings")
+      *
+      (SELECT ("doc"->'events_gaps_perc')::REAL FROM "seed_settings")
+    )::INT AS "following_random_pool"
+  , (
+      SELECT ("doc"->'users_count')::int FROM "seed_settings"
+    ) AS "prog_rand_max"
+)
+, "aggregated_values" AS (
+  SELECT
+    "event_id" AS "target",
+    (
+      SELECT array_agg("user_id")
+      FROM (
+        SELECT "user_id"
+        FROM (
+          SELECT (
+            0 + trunc(random() * (SELECT "prog_rand_max" FROM "seed_info"))::int
+          ) AS "prog_id"
+          FROM generate_series(1, (SELECT "following_random_pool" FROM "seed_info"))
+        ) AS "gs1"
 
--- Populate relation "SentEvents"
--- TRUNCATE "users_sent_events";
--- INSERT INTO "users_sent_events"
--- SELECT
---   (
---     SELECT "user_id" 
---     FROM "users_tmp" 
---     WHERE "gs1" = "gs1" 
---     ORDER BY random()
---     LIMIT 1 
---   ) AS "user_id",
---   (
---     SELECT "event_id" 
---     FROM "all_music_events_temp" 
---     WHERE "gs1" = "gs1" 
---     ORDER BY random()
---     LIMIT 1 
---   ) AS "event_id"
--- FROM generate_series(1, 1000) "gs1"
--- ON CONFLICT ON CONSTRAINT "users_sent_events_pkey"
--- DO NOTHING;
+        JOIN "users_tmp" USING ("prog_id")
+        WHERE "t1"."event_id" = "t1"."event_id"
+
+        LIMIT (
+          floor(random() * ((
+            SELECT "following_max" FROM "seed_info"
+          ) - (
+            SELECT "following_min" FROM "seed_info"
+          ) + 1) + (
+            SELECT "following_min" FROM "seed_info"
+          ))
+        )
+      ) AS "sq1"
+    ) AS "values"
+  FROM "all_music_events_temp" AS "t1"
+)
+, "insert_values" AS (
+  INSERT INTO "public"."users_sent_events"
+  SELECT
+    "value"::uuid AS "user_id"
+  , "target" AS "event_id"
+  FROM "aggregated_values", unnest("values") AS "value"
+  ON CONFLICT ON CONSTRAINT "users_sent_events_pkey"
+  DO NOTHING
+  RETURNING *
+)
+-->> Output >>
+SELECT COUNT(*) AS "inserted_events_artists"
+FROM "insert_values"
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- Cleanup
 DROP TABLE "users_tmp";
