48801a635cfc2876d1d9239c860dadc561178927
marcopeg <marco.pegoraro@gmail.com>
2024-02-23 08:45:41 +0100

    Improve query script

--- projects/pagination/query.sh
+++ projects/pagination/query.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+help() {
+  echo "Description: This script allows you to run parametric queries compatible with the pgbench parameters notation."
+  echo "Usage: $0 -f file.sql -p limit=10 -p page=3 [...options]"
+  echo
+  echo "   -h --help                       Display this help"
+  echo "   -f --file                       Path to the sql file (relative to the script)"
+  echo "   -p --param                      SQL parameters (e.g. -p count=10 -p page=3 -> LIMIT=:count)"
+  echo "   -e --extract                    Extract returning value"
+  echo "   -w --watch                      Re-run the query on change"
+  echo "   -v --verbose                    Verbose mode"
+  echo "   -d --dry-run                    Composes and prints out the query"
+  echo "   --db-name                       Database name (default: postgres)"
+  echo "   --db-user                       Database user (default: postgres)"
+  echo "   --container-name                Docker container name (default: pg)"
+  echo
+  exit 1
+}
+
 is_numeric() {
   if [[ $1 =~ ^[0-9]+$ ]]; then
     echo "true"
@@ -8,74 +26,100 @@ is_numeric() {
   fi
 }
 
-# Default file name, without extension
-FILE_PATH=""
-DOCKER_CONTAINER_NAME="pg"
-DB_USER="postgres"
-DB_NAME="postgres"
-PARAMETERS=()
-VERBOSE=false
-DRY_RUN=false
-EXTRACT=false
-
-
-# Function to display help message
-display_help() {
-  echo "Description: This script allows you to run parametric queries compatible with the pgbench parameters notation."
-  echo "Usage: $0 -f file.sql -p limit=10 -p page=3 [...options]"
-  echo
-  echo "   -f --file FILE_PATH             Path to the file"
-  echo "   -d DB_NAME                      Database name"
-  echo "   -u DB_USER                      Database user"
-  echo "   -n DOCKER_CONTAINER_NAME        Docker container name"
-  echo "   -p --param PARAMETERS           Additional parameters"
-  echo "   -v --verbose                    Verbose mode"
-  echo "   -e --extract                    Extract returning value"
-  echo "   -r --dry-run                    Composes and prints out the query"
-  echo "   -h --help                       Display help"
-  echo
-  exit 1
-}
+FILE_PATH=""         # Default file path (local to the script)
+DK_NAME="pg"         # Default Docker container name
+DB_USER="postgres"   # Default database user
+DB_NAME="postgres"   # Default database name
+PARAMETERS=()        # Array to store parameters (-p count=10 -p page=3)
+VERBOSE=false        # Verbose mode
+DRY_RUN=false        # Dry run mode, prints out the query
+EXTRACT=false        # Extract returning value (first column of the first row)
+WATCH=false          # Watch file for changes and re-run the query
 
 # If no arguments or -h is present, display help
 if [ $# -eq 0 ] || [[ "$@" == *"-h"* ]]; then
-  display_help
+  help
 fi
 
-# Convert long options to short ones
-for arg in "$@"; do
-  shift
-  case "$arg" in
-    "--help") set -- "$@" "-h" ;;
-    "--verbose") set -- "$@" "-v" ;;
-    "--param") set -- "$@" "-p" ;;
-    "--file") set -- "$@" "-f" ;;
-    "--extract") set -- "$@" "-e" ;;
-    "--dry-run") set -- "$@" "-r" ;;
-    "--"*) echo "Unsupported option $arg. Use -h for help." >&2; exit 1 ;;
-    *) set -- "$@" "$arg" ;;
+# Process long-named options
+while (( "$#" )); do
+  case "$1" in
+    "--container-name")
+      DK_NAME="$2"
+      shift 2
+      ;;
+    "--db-name")
+      DB_NAME="$2"
+      shift 2
+      ;;
+    "--db-user")
+      DB_USER="$2"
+      shift 2
+      ;;
+    "--help")
+      set -- "$@" "-h"
+      shift
+      ;;
+    "--verbose")
+      set -- "$@" "-v"
+      shift
+      ;;
+    "--watch")
+      set -- "$@" "-w"
+      shift
+      ;;
+    "--param")
+      set -- "$@" "-p"
+      shift
+      ;;
+    "--file")
+      set -- "$@" "-f"
+      shift
+      ;;
+    "--extract")
+      set -- "$@" "-e"
+      shift
+      ;;
+    "--dry-run")
+      set -- "$@" "-d"
+      shift
+      ;;
+    "--"*)
+      echo "Unsupported option $1. Use -h for help." >&2
+      exit 1
+      ;;
+    *)
+      # If it's not a known option, just append it to the new arguments list
+      NEW_ARGS+=("$1")
+      shift
+      ;;
   esac
 done
 
-# Now, process the remaining options
-while getopts "f:d:u:n:p:vhre" opt; do
+# Set "$@" to NEW_ARGS, containing only the unprocessed arguments
+set -- "${NEW_ARGS[@]}"
+
+# Process single character options
+while getopts "f:p:vhdew" opt; do
   case $opt in
     f) FILE_PATH="$OPTARG";;
-    d) DB_NAME="$OPTARG";;
-    u) DB_USER="$OPTARG";;
-    n) DOCKER_CONTAINER_NAME="$OPTARG";;
     p) PARAMETERS+=("$OPTARG");;
     v) VERBOSE=true;;
-    r) DRY_RUN=true;;
+    d) DRY_RUN=true;;
     e) EXTRACT=true;;
-    h) display_help;;
+    w) WATCH=true;;
+    h) help;;
     \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
   esac
 done
 
 # No file path provided, use default
 if [[ -z "$FILE_PATH" ]]; then
-  echo "Usage: $0 [options] <file-path>" >&2
+  echo "ERROR: File path is required!" >&2
+  echo "       Try this way:" >&2
+  echo "" >&2
+  echo "       $0 -f <file-path> [options]" >&2
+  echo "" >&2
   exit 1
 fi
 
@@ -89,25 +133,26 @@ FILE_TO_MONITOR="$(pwd)/${FILE_PATH}"
 
 # Check if the file exists
 if [ ! -f "$FILE_TO_MONITOR" ]; then
-  echo "Error: \"./${FILE_PATH}\" does not exist." >&2
+  echo "ERROR: \"./${FILE_PATH}\" does not exist." >&2
+  echo "" >&2
   exit 1
 fi
 
-# Construct the parameters string for pgbench
+if [[ $VERBOSE == true ]]; then
+  echo "👉 RUNNING:"
+  echo "${FILE_PATH}"
+  echo ""
+fi
+
+# Build the query out of the input parameters
+# (this will substitute placeholders like ":count" with parameters like "-p count=10")
+QUERY=""
+
 PARAMS_STRING=""
 for param in "${PARAMETERS[@]}"; do
   PARAMS_STRING+=" -D \"$param\""
 done
 
-if [[ $VERBOSE == true ]]; then
-  echo "Running:"
-  echo "${FILE_PATH}"
-  echo ""
-fi
-
-
-# Build the query out of the input parameters
-QUERY=""
 for param in "${PARAMETERS[@]}"; do
   IFS="=" read -r key value <<< "$param"
 
@@ -122,7 +167,7 @@ done
 QUERY=$(cat $FILE_TO_MONITOR | tr '\n' ' ' | eval sed "$QUERY")
 
 if [[ $VERBOSE == true || $DRY_RUN == true ]]; then
-  echo "Query:"
+  echo "👉 QUERY:"
   echo "$QUERY"
   echo ""
 fi
@@ -130,7 +175,57 @@ fi
 # Construct the command
 if [[ $DRY_RUN == false ]]; then
   escaped_query=$(echo "$QUERY" | sed "s/'/'\\\\''/g")
-  COMMAND="docker exec -i $DOCKER_CONTAINER_NAME psql -U $DB_USER -d $DB_NAME -c '$escaped_query'"
+  COMMAND="docker exec -i $DK_NAME psql -U $DB_USER -d $DB_NAME -c '$escaped_query'"
+
+  if [[ $WATCH == true ]]; then
+
+    # Ensure fswatch is installed
+    if ! command -v fswatch &> /dev/null; then
+      echo "WARINING: Please install inotify-tools."
+      echo ""
+      echo "\$> brew install fswatch"
+      echo ""
+      echo "Press ENTER to run the installation command, or ANY KEY to cancel."
+      
+      # Capture user input
+      IFS= read -r -n1 -s input
+      case "$input" in
+        '') # ENTER
+          echo "Installing..."
+          brew install fswatch
+          ;;
+        *)
+          echo "Installation canceled."
+          exit 1
+          ;;
+      esac
+    fi
+
+    if [[ $VERBOSE == true || $DRY_RUN == true ]]; then
+      echo "👉 WATCHING \"./${FILE_PATH}\"."
+      echo "(Ctrl+C to stop)"
+      echo ""
+    fi
+
+    # First execution
+    eval "$COMMAND"
+
+    # Variables to prevent immediate re-execution
+    LAST_EXECUTION_TIME=0
+    EXECUTION_DELAY=1 # Seconds
+
+    fswatch -0 "$FILE_TO_MONITOR" | while IFS= read -r -d "" event; do
+      CURRENT_TIME=$(date +%s)
+      if (( CURRENT_TIME - LAST_EXECUTION_TIME > EXECUTION_DELAY )); then
+        clear
+        echo "👉 [$(date "+%H:%M:%S")] File \"${FILE_PATH}\" has changed"
+        echo ""
+        eval "$COMMAND"
+        LAST_EXECUTION_TIME=$CURRENT_TIME
+      fi
+    done
+    exit 0
+  fi
 
   if [[ $EXTRACT == false ]]; then
     eval "$COMMAND"
--- projects/pagination/watch.sh
+++ projects/pagination/watch.sh
@@ -1,52 +1,5 @@
 #!/bin/bash
 
-# Default file name, without extension
-FILE_PATH=""
-DOCKER_CONTAINER_NAME="pg"
-DB_USER="postgres"
-DB_NAME="postgres"
-
-# First, extract the file path which is not an option (not starting with '-')
-for arg in "$@"; do
-  if [[ "$arg" != -* ]]; then
-    FILE_PATH="$arg"
-    break # Stop at the first non-option argument
-  fi
-done
-
-# Remove the file path from the arguments list
-args=()
-for arg in "$@"; do
-  if [[ "$arg" != "$FILE_PATH" ]]; then
-    args+=("$arg")
-  fi
-done
-set -- "${args[@]}"
-
-# Now, process the remaining options
-while getopts "d:u:n:" opt; do
-  case $opt in
-    d) DB_NAME="$OPTARG";;
-    u) DB_USER="$OPTARG";;
-    n) DOCKER_CONTAINER_NAME="$OPTARG";;
-    \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
-  esac
-done
-
-# No file path provided, use default
-if [[ -z "$FILE_PATH" ]]; then
-  echo "Usage: $0 [options] <file-path>" >&2
-  exit 1
-fi
-
-# Complete file path
-FILE_TO_MONITOR="$(pwd)/${FILE_PATH}"
-
-# Check if the file exists
-if [ ! -f "$FILE_TO_MONITOR" ]; then
-  echo "Error: \"./${FILE_PATH}\" does not exist." >&2
-  exit 1
-fi
 
 # Ensure inotifywait is installed
 if ! command -v fswatch &> /dev/null; then
@@ -70,12 +23,42 @@ if ! command -v fswatch &> /dev/null; then
   esac
 fi
 
-echo "Watching \"./${FILE_PATH}\"."
+# Check if the input file exists
+if [ ! -f "$1" ]; then
+  echo "Input file does not exist"
+  exit 1
+fi
+
+# Store the input file path
+FILE_PATH="$1"
+shift # Remove the first argument so only additional arguments remain
+
+# Complete file path
+FILE_TO_MONITOR="$(pwd)/${FILE_PATH}"
+
+echo "Watching \"${FILE_PATH}\" for changes."
 echo "Ctrl+C to stop."
 echo ""
 
+# Function to execute the script with any additional arguments
+execute_script() {
+  bash "$FILE_TO_MONITOR" "$@"
+}
+
+# Initial execution
+execute_script "$@"
+
+# Variables to prevent immediate re-execution
+LAST_EXECUTION_TIME=0
+EXECUTION_DELAY=1 # Seconds
+
 # Monitor the file for modifications
 fswatch -0 "$FILE_TO_MONITOR" | while IFS= read -r -d "" event; do
-  echo "[$(date "+%H:%M:%S")] File \"${FILE_PATH}\" has changed!"
-  docker exec -i "$DOCKER_CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -f "/sql/${FILE_PATH#*/}"
+  CURRENT_TIME=$(date +%s)
+  if (( CURRENT_TIME - LAST_EXECUTION_TIME > EXECUTION_DELAY )); then
+    clear
+    echo "[$(date "+%H:%M:%S")] File \"${FILE_PATH}\" has changed. Executing..."
+    execute_script "$@"
+    LAST_EXECUTION_TIME=$CURRENT_TIME
+  fi
 done
